13afca49e9a088df143c9020018f628b148461d1
sacha-c <broute.sacha@gmail.com>
2024-11-06 10:38:33 +0100

    fix: fix version bump in main.go again

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 	app := &cli.App{
 		Name:    "sheriff",
 		Usage:   "Fighting dangerous dangerous dependencies since 2024.",
-		Version: "0.12.1",
+		Version: "0.12.4",
 		Flags: []cli.Flag{
 			&cli.BoolFlag{
 				Name:        "verbose",
