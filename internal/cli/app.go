4cf48b955d3f90442c8fbf198840c9a4825546da
sacha <sacha.broute@elementsinteractive.es>
2024-11-27 16:11:08 +0100

    fix: app version 0.17.1

--- internal/cli/app.go
+++ internal/cli/app.go
@@ -9,7 +9,7 @@ func App(args []string) {
 	app := &cli.App{
 		Name:    "sheriff",
 		Usage:   "Fighting dangerous dangerous dependencies since 2024.",
-		Version: "0.19.3",
+		Version: "0.17.1",
 		Commands: []*cli.Command{
 			{
 				Name:  "patrol",
