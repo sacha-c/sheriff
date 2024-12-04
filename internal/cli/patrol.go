226cc8d51a64a6e88b3f4af15216d84ab59667c1
Sergio Castillo <s.cast.lara@gmail.com>
2024-12-04 16:40:29 +0100

    fix: change cli message on warn to "patrol" and not "scan" (#18)
    * fix: change cli message on warn to "patrol" and not "scan"
    
    * chore: update internal/cli/patrol.go
    
    Co-authored-by: Sacha Brouté <3247529+sacha-c@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Sacha Brouté <3247529+sacha-c@users.noreply.github.com>

--- internal/cli/patrol.go
+++ internal/cli/patrol.go
@@ -143,7 +143,7 @@ func PatrolAction(cCtx *cli.Context) error {
 
 	patrolService := patrol.New(gitlabService, slackService, gitService, osvService)
 
-	// Run the scan
+	// Do the patrol
 	if warn, err := patrolService.Patrol(
 		cCtx.StringSlice(groupsFlag),
 		cCtx.StringSlice(projectsFlag),
@@ -155,7 +155,7 @@ func PatrolAction(cCtx *cli.Context) error {
 	); err != nil {
 		return errors.Join(errors.New("failed to scan"), err)
 	} else if warn != nil {
-		return cli.Exit("Scan was partially successful, some errors occurred. Check the logs for more information.", 1)
+		return cli.Exit("Patrol was partially successful, some errors occurred. Check the logs for more information.", 1)
 	}
 
 	return nil
